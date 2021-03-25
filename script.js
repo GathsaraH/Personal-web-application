170339a445b0591109bb86364ab3af2c26ea3b40
Gathsara Umesh <umeshgathsara@gmail.com>
2021-03-25 15:52:38 +0530

    Make Script Link

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
-<script src='assest/bootstrap-5.0.0-beta1-dist/js/bootstrap.min.js'></script>
-
-
+function forPageOne() {
+  document.getElementById('MainPage').style.zIndex = 200
+  document.getElementById('About-Me').style.zIndex = -200
+  document.getElementById('Service').style.zIndex = -200
+  document.getElementById('Skill').style.zIndex = -200
+  document.getElementById('Contact').style.zIndex = -200
+}
+function forPage02() {
+  document.getElementById('MainPage').style.zIndex = -200
+  document.getElementById('About-Me').style.zIndex = 200
+  document.getElementById('Service').style.zIndex = -200
+  document.getElementById('Skill').style.zIndex = -200
+  document.getElementById('Contact').style.zIndex = -200
+}
+function forPage03() {
+  document.getElementById('MainPage').style.zIndex = -200
+  document.getElementById('About-Me').style.zIndex = -200
+  document.getElementById('Service').style.zIndex = 200
+  document.getElementById('Skill').style.zIndex = -200
+  document.getElementById('Contact').style.zIndex = -200
+}
+function forPage04() {
+  document.getElementById('Skill').style.zIndex = 200
+  document.getElementById('MainPage').style.zIndex = -200
+  document.getElementById('About-Me').style.zIndex = -200
+  document.getElementById('Service').style.zIndex = -200
+  document.getElementById('Contact').style.zIndex = -200
+}
+function forPage05() {
+  document.getElementById('Contact').style.zIndex = 200
+  document.getElementById('Skill').style.zIndex = -200
+  document.getElementById('MainPage').style.zIndex = -200
+  document.getElementById('About-Me').style.zIndex = -200
+  document.getElementById('Service').style.zIndex = -200
+}
